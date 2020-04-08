bc58705e6d25b7688b19b4aac74c7c53ca81228c
Jared Gregor <32880933+jmgregor@users.noreply.github.com>
2020-04-08 11:49:40 -0400

    Unity

---
+++ Putt Putt Golf/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject golfBall;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - golfBall.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = golfBall.transform.position + offset;
+    }
+}
