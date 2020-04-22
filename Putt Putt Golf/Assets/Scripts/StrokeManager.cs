e76998b54a899f876c889689b779db52c91d2af8
Jared Gregor <32880933+jmgregor@users.noreply.github.com>
2020-04-21 23:03:43 -0400

    Game Mechanics

--- Putt Putt Golf/Assets/Scripts/StrokeManager.cs
+++ Putt Putt Golf/Assets/Scripts/StrokeManager.cs
@@ -10,6 +10,11 @@ public class StrokeManager : MonoBehaviour
     private bool buttonPress = false;
     private Rigidbody playerBallRB;
     public float StrokeAngle { get; protected set; }
+    public float StrokeRate;
+    private float nextStroke = 0.0f;
+
+    public enum StrokeModeEnum { STOPPED, ROLLING };
+    public StrokeModeEnum StrokeMode { get; protected set; }
 
     // Start is called before the first frame update
     void Start()
@@ -20,21 +25,36 @@ public class StrokeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time > nextStroke && StrokeMode == StrokeModeEnum.STOPPED)
         {
+            nextStroke = Time.time + StrokeRate;
             buttonPress = true;
         }
 
         StrokeAngle += Input.GetAxis("Horizontal") * 100f * Time.deltaTime;
     }
     
+    void UpdateStrokeMode()
+    {
+        if (playerBallRB.IsSleeping())
+        {
+            StrokeMode = StrokeModeEnum.STOPPED;
+        }
+    }
+
     void FixedUpdate()
     {
+        if (StrokeMode == StrokeModeEnum.ROLLING)
+        {
+            UpdateStrokeMode();
+            return;
+        }
         if (buttonPress)
         {
             buttonPress = false;
-            Vector3 forceVec = new Vector3(0, 0, 1f);
+            Vector3 forceVec = new Vector3(0, 0, 10f);
             playerBallRB.AddForce(Quaternion.Euler(0, StrokeAngle, 0) * forceVec, ForceMode.Impulse);
+            StrokeMode = StrokeModeEnum.ROLLING;
         } 
     }
 }
