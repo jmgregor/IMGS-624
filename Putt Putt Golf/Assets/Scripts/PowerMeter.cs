f4eaa452c598e93274c30f8f5238a8ea654fff5d
Jared Gregor <32880933+jmgregor@users.noreply.github.com>
2020-04-22 17:20:01 -0400

    PowerMeter

---
+++ Putt Putt Golf/Assets/Scripts/PowerMeter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerMeter : MonoBehaviour
+{
+    StrokeManager StrokeManager;
+    Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StrokeManager = GameObject.FindObjectOfType<StrokeManager>();
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = StrokeManager.powerPercent;
+    }
+}
--- Putt Putt Golf/Assets/Scripts/StrokeManager.cs
+++ Putt Putt Golf/Assets/Scripts/StrokeManager.cs
@@ -7,13 +7,18 @@ using UnityEngine;
 public class StrokeManager : MonoBehaviour
 {
     public GameObject ball;
-    private bool buttonPress = false;
     private Rigidbody playerBallRB;
     public float StrokeAngle { get; protected set; }
-    public float StrokeRate;
-    private float nextStroke = 0.0f;
 
-    public enum StrokeModeEnum { STOPPED, ROLLING };
+    //Power Meter
+    public float StrokePower { get; protected set; }
+    float maxPower = 100f;
+    public float powerPercent { get { return StrokePower / maxPower; } }
+    public float fillTime = 10f;
+    int fill = 1;
+
+
+    public enum StrokeModeEnum { PUTT, POWERING, AIMING, ROLLING };
     public StrokeModeEnum StrokeMode { get; protected set; }
 
     // Start is called before the first frame update
@@ -25,20 +30,46 @@ public class StrokeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextStroke && StrokeMode == StrokeModeEnum.STOPPED)
+        if(StrokeMode == StrokeModeEnum.AIMING)
         {
-            nextStroke = Time.time + StrokeRate;
-            buttonPress = true;
+            //change putt angle
+            StrokeAngle += Input.GetAxis("Horizontal") * 100f * Time.deltaTime;
+
+            //switch to powering up
+            if (Input.GetButtonUp("Fire1"))
+            {
+                StrokeMode = StrokeModeEnum.POWERING;
+                return;
+            }
+
         }
 
-        StrokeAngle += Input.GetAxis("Horizontal") * 100f * Time.deltaTime;
+        if(StrokeMode == StrokeModeEnum.POWERING)
+        {
+            //power meter fills up and back down
+            StrokePower += (fillTime * fill) * Time.deltaTime;
+            if(StrokePower > maxPower)
+            {
+                StrokePower = maxPower;
+                fill = -1;
+            } else if (StrokePower < 0)
+            {
+                StrokePower = 0;
+                fill = 1;
+            }
+            //if hits button, shoot
+            if (Input.GetButtonUp("Fire1"))
+            {
+                StrokeMode = StrokeModeEnum.PUTT;
+            }
+        }
     }
     
-    void UpdateStrokeMode()
+    void checkRolling()
     {
         if (playerBallRB.IsSleeping())
         {
-            StrokeMode = StrokeModeEnum.STOPPED;
+            StrokeMode = StrokeModeEnum.AIMING;
         }
     }
 
@@ -46,15 +77,15 @@ public class StrokeManager : MonoBehaviour
     {
         if (StrokeMode == StrokeModeEnum.ROLLING)
         {
-            UpdateStrokeMode();
+            checkRolling();
             return;
-        }
-        if (buttonPress)
+        } else if (StrokeMode == StrokeModeEnum.PUTT)
         {
-            buttonPress = false;
-            Vector3 forceVec = new Vector3(0, 0, 10f);
-            playerBallRB.AddForce(Quaternion.Euler(0, StrokeAngle, 0) * forceVec, ForceMode.Impulse);
             StrokeMode = StrokeModeEnum.ROLLING;
+            Vector3 forceVec = new Vector3(0, 0, StrokePower);
+            playerBallRB.AddForce(Quaternion.Euler(0, StrokeAngle, 0) * forceVec, ForceMode.Impulse);
+            StrokePower = 0;
+            fill = 1;
         } 
     }
 }
