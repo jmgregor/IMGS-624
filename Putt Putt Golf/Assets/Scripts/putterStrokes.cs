9aa568ce999e5eaeef759487448aeca2a73aaab1
Jared Gregor <32880933+jmgregor@users.noreply.github.com>
2020-04-25 16:35:31 -0400

    Putter Stroke Count

---
+++ Putt Putt Golf/Assets/Scripts/putterStrokes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class putterStrokes : MonoBehaviour
+{
+    StrokeManager StrokeManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StrokeManager = GameObject.FindObjectOfType<StrokeManager>();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //winText.text = "Game Over";
+            StrokeManager.StrokeCount++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
